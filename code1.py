d000c19e27183f16fcbcb7ef5f32297243fec030
daliaibrahim58 <daliaibrahim582005@gmail.com>
2026-02-10 16:23:51 +0200

    first commit

--- code1.py
+++ code1.py
@@ -1,37 +1,29 @@
-import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-lst = [12.23, 13.32, 100, 36.32]
-arr = np.array(lst)
-print("Original List:", lst)
-print("One-dimensional NumPy array:", arr)
-
-matrix = np.arange(2, 11).reshape(3, 3)
-print(matrix)
-
-arr_int = np.array([1, 2, 3, 4])
-print(arr_int.astype(float))
-
-arr2 = np.array([[1, 4, 2],
-                 [3, 0, 5]])
-print(np.argmax(arr2, axis=1))
-print(np.argmin(arr2, axis=1))
-
-a = np.array([1, 2])
-b = np.array([4, 5])
-print(a > b)
-print(a >= b)
-print(a < b)
-print(a <= b)
-
-df = pd.read_csv('./data.csv')
+df = pd.read_csv('./data2.csv')
 
 print(df.head())
 print(df.info())
+print(df.isnull().sum())
 print(df.describe())
 
-numeric_cols = df.select_dtypes(include='number').columns
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=df[['age', 'bmi']])
+plt.show()
 
-df['Total'] = df[numeric_cols[0]] + df[numeric_cols[1]]
-print(df[df[numeric_cols[0]] > 120])
-print(df.sort_values(by=numeric_cols[0], ascending=True))
+plt.figure(figsize=(8, 5))
+sns.countplot(x='smoker', data=df)
+plt.show()
+
+le = LabelEncoder()
+df['sex'] = le.fit_transform(df['sex'])
+df['smoker'] = le.fit_transform(df['smoker'])
+df['region'] = le.fit_transform(df['region'])
+
+scaler = MinMaxScaler()
+df[['age', 'bmi']] = scaler.fit_transform(df[['age', 'bmi']])
+
+print(df.head())
