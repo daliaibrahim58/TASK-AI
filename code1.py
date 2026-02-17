a81e129f48b63082eb837fbc6a34677ef5bddfba
daliaibrahim58 <daliaibrahim582005@gmail.com>
2026-02-17 19:06:25 +0200

    task 4

--- code1.py
+++ code1.py
@@ -1,61 +1,98 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-df = pd.read_csv("./house_data.csv")
 
-df["date"] = pd.to_datetime(df["date"])
-df["year"] = df["date"].dt.year
-df["month"] = df["date"].dt.month
-df = df.drop(["id", "date"], axis=1)
 
-target = "price"
+df = pd.read_csv("./credit_card_fraud.csv")
 
-num_df = df.select_dtypes(include=[np.number])
+print("First rows:")
+print(df.head())
 
-Q1 = num_df.quantile(0.25)
-Q3 = num_df.quantile(0.75)
-IQR = Q3 - Q1
-df = df[~((num_df < (Q1 - 1.5 * IQR)) | (num_df > (Q3 + 1.5 * IQR))).any(axis=1)]
+print("\nShape:")
+print(df.shape)
 
-y = df[target]
-X = df.drop(target, axis=1)
+print("\nInfo:")
+print(df.info())
 
-plt.figure(figsize=(6,4))
-sns.histplot(y, kde=True)
-plt.show()
 
-plt.figure(figsize=(12,10))
-sns.heatmap(df.corr(), cmap="coolwarm")
-plt.show()
+print("\nSummary statistics:")
+print(df.describe())
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+print("\nMode:")
+print(df.mode().iloc[0])
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
+outlier_columns = []
+
+for col in df.select_dtypes(include=np.number).columns:
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    if ((df[col] < lower) | (df[col] > upper)).any():
+        outlier_columns.append(col)
+
+print("\nColumns with outliers:")
+print(outlier_columns)
+
+
+print("\nClass distribution:")
+print(df["is_fraud"].value_counts())
+
+print("\nClass percentage:")
+print(df["is_fraud"].value_counts(normalize=True))
+
+
+
+categorical_cols = df.select_dtypes(include="object").columns
+df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
+
+
+for col in outlier_columns:
+    if col in df.columns and col != "is_fraud":
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower = Q1 - 1.5 * IQR
+        upper = Q3 + 1.5 * IQR
+
+        df = df[(df[col] >= lower) & (df[col] <= upper)]
+
+
+X = df.drop("is_fraud", axis=1)
+y = df["is_fraud"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    stratify=y,
+    random_state=42
+)
+
+
+
+model = LogisticRegression(max_iter=1000)
+
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-print("MSE:", mse)
-print("R2:", r2)
-
-plt.figure(figsize=(6,5))
-plt.scatter(y_test, y_pred)
-plt.xlabel("Actual")
-plt.ylabel("Predicted")
-plt.show()
-
-print("The model shows a strong relationship between house features and price.")
-print("The performance can be improved using feature engineering and advanced models.")
+print("\nResults:")
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
